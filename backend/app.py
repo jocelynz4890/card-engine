f8908f47b618e92114efe7c267e9dff252a31deb
jocelyn <jocelynz4890@gmail.com>
2025-03-29 17:01:01 -0400

    might work

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -7,3 +8,8 @@ CORS(app)
 @app.route("/")
 def Home():
     return "Hi"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
