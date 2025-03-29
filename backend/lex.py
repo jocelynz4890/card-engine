37b1632423a7d0d4e6013b7065fa3cdd15795c62
Mikhail Kornilov <zcp3re@virginia.edu>
2025-03-29 17:21:01 -0400

    start lexer

---
+++ backend/lex.py
@@ -0,0 +1,21 @@
+import ply.lex as lex
+tokens = (
+  'NUMBER',
+  'PLUS',
+  'MINUS',
+  'TIMES',
+  'DIVIDE',
+  'ASSIGN',
+  'LPAREN',
+  'RPAREN',
+  'N',
+  'IN',
+  'FOR',
+  'PLAYER',
+  'ON',
+  'INDENT',
+  'SUIT',
+  'RANK',
+  'ID',
+
+)
