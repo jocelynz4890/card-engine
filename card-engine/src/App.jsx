6b47a5384ff1b71098a07102e4d9ad79eb58cbe5
jocelyn <jocelynz4890@gmail.com>
2025-03-29 19:05:54 -0400

    frontend setup

--- card-engine/src/App.jsx
+++ card-engine/src/App.jsx
@@ -4,31 +4,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-max h-max p-4">
+      <h1 className="text-3xl font-bold mt-0 mb-4">♣️ 🂡 ♥️ Custom Card Game Engine ♦️ 🂡 ♠️ </h1>
+      <div className= "flex flex-row items-start w-1/2 h-1/2 flex-row gap-4">
+        <div className="interface w-1/2 h-1/2 border gap-4">gjioewhioewgjiowjojtio34j
+          gwn4iohiogj3
+          gnwoi4hgtowijgiohrgio42h
+          gehiog4hwoigjo4jrg
+          ngwiouhoijgirgh4
+          trgniow4hgoi34j34g
+          engio4h2o4ijt43t
+          gnio2hg4ioj5trowt
+          3ngow4iojto24jt943htg
+          wngio24hgoijgopiwg54
+        </div>
+        <div className="interface w-1/2 h-1/2 border gap-4">
+          wioi4wghi4ogioerjg
+          ngwiot3ji4ojgiporjg
+          wohegi4wogjiregji4pewg
+          nwioefjiow4jgio4gjo5
+          weofgwhiogji4ogjoi4g
+          wfmeiogji4owjgi4og
+          wo4gnioe5jiohgji5oh
+          wogiow4jgio4g
+        </div>
+        <button>Export Game</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
